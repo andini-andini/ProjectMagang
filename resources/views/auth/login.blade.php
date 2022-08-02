ced1917acc8c55f1b31d24e547a8bf9432995f28
= <=>
2022-08-02 16:48:00 +0700

    Membuat Database

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -70,4 +70,119 @@
         </div>
     </div>
 </div>
-@endsection
+@endsection --}}
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="Template/login/fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="Template/login/css/owl.carousel.min.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="Template/login/css/bootstrap.min.css">
+
+    <!-- Style -->
+    <link rel="stylesheet" href="Template/login/css/style.css">
+
+    <title>Login</title>
+</head>
+
+<body>
+
+
+
+    <div class="content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="Template/login/images/undraw_remotely_2j6y.svg" alt="Image" class="img-fluid">
+                </div>
+                <div class="col-md-6 contents">
+                    <div class="row justify-content-center">
+                        <div class="col-md-8">
+                            <div class="mb-4">
+                                <h3>Sign In</h3>
+                                {{-- <p class="mb-4">Lorem ipsum dolor sit amet elit. Sapiente sit auteos consectetur adipisicing.</p> --}}
+                            </div>
+                            <form action="{{ route('login') }}" method="post">
+                                @csrf
+                                <div class="form-group first">
+                                    <label for="email">{{ __('Email Address') }}</label>
+                                    <input id="email" type="email"
+                                        class="form-control @error('email') is-invalid @enderror" name="email"
+                                        value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+
+                                </div>
+                                <div class="form-group last mb-4">
+                                    <label for="password">{{ __('Password') }}</label>
+                                    {{-- <input type="password" class="form-control" id="password"> --}}
+                                    <input id="password" type="password"
+                                        class="form-control @error('password') is-invalid @enderror" name="password"
+                                        required autocomplete="current-password">
+
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+
+                                </div>
+
+                                <div class="d-flex mb-5 align-items-center">
+                                    <label class="control control--checkbox mb-0"><span class="caption">Remember
+                                            me</span>
+                                        <input type="checkbox" checked="checked" />
+                                        <div class="control__indicator"></div>
+                                    </label>
+                                    <span class="ml-auto"><a href="#" class="forgot-pass">Forgot
+                                            Password</a></span>
+                                </div>
+
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
+                                {{-- <input type="submit" value="Log In" class="btn btn-block btn-primary"> --}}
+
+                                {{-- <span class="d-block text-left my-4 text-muted">&mdash; or login with
+                                    &mdash;</span>
+
+                                <div class="social-login">
+                                    <a href="#" class="facebook">
+                                        <span class="icon-facebook mr-3"></span>
+                                    </a>
+                                    <a href="#" class="twitter">
+                                        <span class="icon-twitter mr-3"></span>
+                                    </a>
+                                    <a href="#" class="google">
+                                        <span class="icon-google mr-3"></span>
+                                    </a>
+                                </div> --}}
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="Template/login/js/jquery-3.3.1.min.js"></script>
+    <script src="Template/login/js/popper.min.js"></script>
+    <script src="Template/login/js/bootstrap.min.js"></script>
+    <script src="Template/login/js/main.js"></script>
+</body>
+
+</html>
